95ba4fa65f261f0da1fbfbc3aea8ea56c2157c3c
Nuvio iOS Developer <ios-dev@nuvio.local>
2026-08-26 06:44:35 +0000

    fix(imports): add missing fillMaxWidth import in SwipeBackGesture

--- composeApp/src/commonMain/kotlin/com/nuvio/app/core/ui/SwipeBackGesture.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/core/ui/SwipeBackGesture.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
